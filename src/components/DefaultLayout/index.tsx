e25db4f012f41941b4c12aacaab5eb47c713ea14
양찬규 <yangchan00@ship-da.com>
2024-01-22 18:52:50 +0900

    [chore] Layout 파일 추가

---
+++ src/components/DefaultLayout/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const DefaultLayoutWrapper = styled.div``;
+
+function DefaultLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <DefaultLayoutWrapper>{children}</DefaultLayoutWrapper>
+    </>
+  );
+}
+
+export default DefaultLayout;
