e8bcc2707879032e679405edb9a44e0c584648b7
undefineddev2024 <undefineddev2024@gmail.com>
2024-06-16 05:41:27 +0000

    Update from https://github.com/undefined-dev-ko/ship-check-fe/commit/5e0f75e2b0507239dbc88851b909749b337ec0c5

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,8 +1,7 @@
 type EnvKeys =
   | 'REACT_APP_NODE_ENV'
   | 'REACT_APP_BASE_API_URL'
-  | 'REACT_APP_GOOGLE_CLIENT_ID'
-  | 'REACT_APP_GOOGLE_REDIRECT_URI';
+  | 'REACT_APP_GOOGLE_CLIENT_ID';
 
 const getEnv = (envName: EnvKeys, required = false): string => {
   const result = process.env[envName];
@@ -17,5 +16,10 @@ const getEnv = (envName: EnvKeys, required = false): string => {
 export const getBaseApiUrl = () => getEnv('REACT_APP_BASE_API_URL', true);
 export const getGoogleClientId = () =>
   getEnv('REACT_APP_GOOGLE_CLIENT_ID', true);
-export const getGoogleRedirectURI = () =>
-  getEnv('REACT_APP_GOOGLE_REDIRECT_URI', true);
+export const getGoogleRedirectURI = () => {
+  // ex) http://localhost:3000/auth/google
+  const hostname = window.location.hostname;
+  const protocol = window.location.protocol;
+  const port = window.location.port ? `:${window.location.port}` : '';
+  return `${protocol}//${hostname}${port}/auth/google`;
+};
