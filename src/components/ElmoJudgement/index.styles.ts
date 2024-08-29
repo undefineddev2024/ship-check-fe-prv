ad740969b985a86f8a8c7730faf92819289ef1ef
undefineddev2024 <undefineddev2024@gmail.com>
2024-08-29 11:39:48 +0000

    Update from https://github.com/undefined-dev-ko/ship-check-fe/commit/e6d196df1db563e8e173683ca77b8ec3f7b9deb9

--- src/components/ElmoJudgement/index.styles.ts
+++ src/components/ElmoJudgement/index.styles.ts
@@ -35,8 +35,11 @@ const NameListContainer = styled.div`
 `;
 
 const NameTag = styled.div`
-  font-size: 30px;
-  color: yellow;
+  font-size: 40px;
+  // burning effect
+  color: #fff;
+  text-shadow: 0px -1px 4px white, 0px -2px 10px yellow, 0px -10px 20px #ff8000,
+    0px -18px 40px red;
 `;
 
 export default {
